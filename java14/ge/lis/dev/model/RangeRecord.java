1b1d2acbaf133d5ec6bb1d671fc2bf42476d4cf6
lis <foxigs@gmail.com>
2020-08-07 15:47:09 +0400

    formatting

--- java14/ge/lis/dev/model/RangeRecord.java
+++ java14/ge/lis/dev/model/RangeRecord.java
@@ -2,7 +2,7 @@ package ge.lis.dev.model;
 
 import java.io.Serializable;
 
-public record RangeRecord (int lo, int hi) implements Serializable {
+public record RangeRecord(int lo, int hi) implements Serializable {
   public RangeRecord {
     if (lo > hi)
       throw new IllegalArgumentException(String.format("%d, %d", lo, hi));
